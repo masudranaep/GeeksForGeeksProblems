990cb76ca6a6d417acd94dbe7a1d9de32ee2d7e5
Masud <mrmasudranaep@gmail.com>
2021-02-04 22:53:49 +0600

    Set Bits

---
+++ Set_Bits.java
@@ -0,0 +1,17 @@
+class Solution{
+    static int setBits(int N){
+        
+// code here
+
+       
+ int count = 0; 
+      
+  while (N > 0) { 
+        
+    count += N & 1; 
+          
+  N >>= 1; 
+        } 
+    
+    return count; 
+    } 
