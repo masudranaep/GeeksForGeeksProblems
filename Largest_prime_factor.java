ff5ebb896630bde5a2af011894a729ee68d89289
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-02-15 08:53:54 +0600

    Largest prime factor

---
+++ Largest_prime_factor.java
@@ -0,0 +1,36 @@
+   static long largestPrimeFactor(int N) {
+  
+      // code here
+ 
+long maxPrime = -1;
+ 
+while(N % 2 == 0){
+    
+ maxPrime = 2;
+   
+  N >>= 1;
+ 
+}
+ 
+for(int i = 3; i <= Math.sqrt(N); i += 2){
+
+     while(N % i == 0){
+ 
+        maxPrime = i;
+ 
+        N = N /i;
+ 
+    }
+
+ }
+
+ if(N > 2)
+ 
+    maxPrime = N;
+ 
+    return  maxPrime;
+ 
+
+    }
+
+}
