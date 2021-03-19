18eaaca43fe1da8067de0182a44c100d3692dd68
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-03-19 09:13:57 +0600

    Reverse a String

---
+++ Reverse a String .java
@@ -0,0 +1,32 @@
+ public static String reverseWord(String str)
+    {
+ 
+       // Reverse the string str
+    
+        int n = str.length();
+    
+    if(n == 0)
+      
+      return " ";
+  
+      int l = 0, r = n-1;
+    
+    char[] ch = str.toCharArray();
+ 
+       while(l < r){
+    
+        char c = ch[l];
+   
+         ch[l] = ch[r];
+       
+     ch[r] = c;
+     
+       l++;
+   
+         r--;
+    
+}
+      
+  return new String(ch);
+    
+    }
