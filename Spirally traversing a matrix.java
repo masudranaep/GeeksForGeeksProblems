671701235f940a6dc64c783403f419251e5cfc3d
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-04-01 07:44:16 +0600

    Spirally traversing a matrix

---
+++ Spirally traversing a matrix.java
@@ -0,0 +1,72 @@
+
+    static ArrayList<Integer> spirallyTraverse(int matrix[][], int r, int c)
+ 
+   {
+        // code here 
+
+ 
+int top = 0;
+
+ int bottom = r - 1;
+ 
+int left = 0;
+ 
+int right = c - 1;
+
+ 
+ArrayList<Integer> res = new ArrayList();
+ 
+//if(matrix.length == 0) return res;
+
+ 
+  while(top <= bottom && left <= right){
+  
+    
+      for(int i = left; i <= right; i++){
+ 
+         res.add(matrix[top][i]);
+   
+   }
+      
+ top++;
+ 
+
+      for(int i = top; i <= bottom; i++){
+ 
+          res.add(matrix[i][right]);
+ 
+      }
+      
+ right--;
+  
+     if(top <= bottom){
+  
+         for(int i = right; i >= left; i--){
+
+               res.add(matrix[bottom][i]);
+   
+        }
+       }
+  
+     bottom--;
+  
+     if(left <= right){
+ 
+          for(int i = bottom; i >= top; i--){
+ 
+
+              res.add(matrix[i][left]);
+ 
+          }
+       }
+  
+     left++;
+    
+   
+  }
+ 
+ return res;
+ 
+
+    }
+}
