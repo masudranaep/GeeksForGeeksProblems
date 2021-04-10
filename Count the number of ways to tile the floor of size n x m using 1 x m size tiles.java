5f330c5f60c37050aab74055cd196f4bc7d9cf4a
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-04-10 07:53:06 +0600

    Count the number of ways to tile the floor of size n x m using 1 x m size tiles

---
+++ Count the number of ways to tile the floor of size n x m using 1 x m size tiles.java
@@ -0,0 +1,34 @@
+    public int countWays(int n, int m)
+    {
+    
+    if(n < m) return 0;
+   
+     // Code here
+
+         int count[] = new int[n+1];
+ 
+  count[0] = 0;
+
+   int i;
+ 
+  for(i = 1; i <= n; i++){
+    
+   if(i > m)
+    
+       count[i] = count[i - 1] + count[i - m];
+ 
+ 
+    
+       else if(i < m || i == 1)
+      
+     count[i] = 1;
+ 
+      else
+        
+   count[i] = 2;
+
+   }
+
+   return count[n];
+    
+    }
