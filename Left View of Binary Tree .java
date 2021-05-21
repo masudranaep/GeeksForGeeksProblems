96bc318e6d7155cd64f07933aae0c1cd22ef1659
Md Masud Rana <77980851+masudranaep@users.noreply.github.com>
2021-05-21 09:31:40 +0600

    Left View of Binary Tree

---
+++ Left View of Binary Tree .java
@@ -0,0 +1,25 @@
+class Tree{
+  
+  Map<Integer,Integer> map = new TreeMap<>();
+  
+  void dfs(Node root, int level){
+   
+     if(root == null) return;
+     
+   if(!map.containsKey(level)) map.put(level,root.data);
+ 
+       dfs(root.left, level+1);
+  
+      dfs(root.right, level+1);
+  
+  }
+ 
+   ArrayList<Integer> leftView(Node root){
+  
+      dfs(root,0);
+       
+ return new ArrayList<>(map.values());
+ 
+   }
+
+}
